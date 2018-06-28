0a327952c20eefcca3deeef814a93da0ab734190
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-06-28 03:06:39 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  board[index] == " " || ""
+end
+
+def position_taken? (board, index)
+   board[index] == "X" || board[index] == "O"
+   if board[index] != "  "
+     return false
+   elsif board[index] == " string "
+     return true
+   end
+end
